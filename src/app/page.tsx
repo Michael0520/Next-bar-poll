400487a67ea80a11e5f5cc32adce461b38ece82e
Michael Lo <po2319987@gmail.com>
2024-03-03 17:29:42 +0800

    Add theme provider and dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import BarPoll from "@/components/bar-poll";
+import { ModeToggle } from "@/components/darkmode";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center container mx-auto">
+    <main className="flex min-h-screen flex-col items-center justify-center container mx-auto gap-4">
+      <div className='flex justify-end items-end w-full'>
+        <ModeToggle />
+      </div>
       <BarPoll/>
     </main>
   );
